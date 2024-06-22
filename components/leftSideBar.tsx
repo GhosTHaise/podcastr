5f8dbf3453a6a2794d4218d68078f51e49ea9263
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-22 19:42:21 +0300

    feat : leftsidebar logo

--- components/leftSideBar.tsx
+++ components/leftSideBar.tsx
@@ -6,13 +6,16 @@ const LeftSideBar = () => {
     return (
         <section className='left_sidebar'>
             <nav className='flex flex-col gap-6'>
-                <Link href="/">
+                <Link
+                    className='flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'
+                    href="/">
                     <Image
-                        src="/icons/logo/logo.svg"
+                        src="/icons/logo.svg"
                         alt='logo'
                         width={23}
                         height={27}
                     />
+                    <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Podcastr</h1>
                 </Link>
             </nav>
         </section>
