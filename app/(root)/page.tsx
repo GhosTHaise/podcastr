7c68b3fd5af3558dbaf851952f9cf8178c8aaee0
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-24 20:51:21 +0300

    feat : create schema

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import PodcastCard from '@/components/podcastCard'
-import { Button } from '@/components/ui/button'
 import { podcastData } from '@/constants'
-import React from 'react'
+import { useQuery } from "convex/react";
+import { api } from '@/convex/_generated/api';
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div
       className='mt-9 flex flex-col gap-9'>
@@ -13,6 +15,9 @@ const Home = () => {
           className="text-20 font-bold text-white-1">
           Trending Podcasts
         </h1>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+          {tasks?.map(({ _id, text }: { _id: string, text: string }) => <div key={_id}>{text}</div>)}
+        </main>
         <div className='podcast_grid'>
           {
             /* id,title,description,imgURL */
