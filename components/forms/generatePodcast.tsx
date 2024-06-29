b612e0af7f1ba3d146b2d2aaca761fd9b1b86853
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-29 19:45:36 +0300

    textarea generate podcast

--- components/forms/generatePodcast.tsx
+++ components/forms/generatePodcast.tsx
@@ -1,5 +1,7 @@
 import { GeneratePodcastProps } from '@/types'
+import { Label } from '@radix-ui/react-label'
 import React from 'react'
+import { Textarea } from '../ui/textarea'
 
 const GeneratePodcast = ({
     setAudioStorageId,
@@ -10,7 +12,25 @@ const GeneratePodcast = ({
     setVoicePrompt,
     setAudioDuration }: GeneratePodcastProps) => {
     return (
-        <div>GeneratePodcast</div>
+        <div>
+            <div className='flex flex-col gap-2.5'>
+                <Label className='text-16 font-bold text-white-1'>
+                    AI Prompt to generate Podcast
+                </Label>
+                <Textarea
+                    className='input-class font-light focus-visible:ring-orange-1'
+                    placeholder='Provide text to generate audio'
+                    rows={5}
+                    value={voicePrompt}
+                    onChange={(e) => setVoicePrompt(e.target.value)}
+                >
+
+                </Textarea>
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
 
